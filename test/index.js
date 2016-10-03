be27025e0a97efcafd1d6fc041d4d624ca44cabd
oizhaolei <oizhaolei@gmail.com>
2016-10-03 20:21:46 +0800

    const -> var

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 'use strict';
 var assert = require('assert');
 
-const Sudiyi = require('../lib/index.js');
+var Sudiyi = require('../lib/index.js');
 var sudiyi = new Sudiyi({
   partner_id: process.env.PARTNER_ID,
   partner_key: process.env.PARTNER_KEY,
@@ -12,14 +12,18 @@ describe('module', function () {
 
   it('area', function (done) {
     sudiyi.area().then(function (data) {
+      data = JSON.parse(data);
       console.log(data);
+
       done();
     });
   });
 
   it('boxStatus', function (done) {
     sudiyi.boxStatus(1006865).then(function (data) {
+      data = JSON.parse(data);
       console.log(data);
+
       done();
     });
   });
